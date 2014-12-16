950fab302739e5ac695ef2c29026433d77ed51e9
Scott Zeid <s@zeid.me>
2014-12-16 03:54:31 -0600

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+
+# This file is public domain via CC0:
+# <https://creativecommons.org/publicdomain/zero/1.0/>
+
+from setuptools import setup, find_packages
+
+setup(
+ name="iosapplist",
+ version="3.0.dev1",
+ description="Lists installed iOS App Store apps.",
+ url="http://code.s.zeid.me/iosapplist",
+ author="Scott Zeid",
+ author_email="s@zeid.me",
+ license="X11 License:  https://tldrlegal.com/license/x11-license",
+ classifiers=[
+  "Development Status :: 3 - Alpha",
+  "Environment :: Console",
+  "Intended Audience :: Developers",
+  "Intended Audience :: System Administrators",
+  "Natural Language :: English",
+  "Operating System :: iOS",
+  "Operating System :: MacOS :: MacOS X",
+  "Operating System :: POSIX",
+  "Operating System :: POSIX :: Linux",
+  "Programming Language :: Python :: 2",
+  "Programming Language :: Python :: 2.5",
+  "Programming Language :: Python :: 2 :: Only",
+  "Topic :: Software Development :: Libraries",
+  "Topic :: Software Development :: Libraries :: Python Modules",
+  "Topic :: System :: Systems Administration",
+  "Topic :: Utilities",
+ ],
+ packages=find_packages(),
+ install_requires=["CFPropertyList", "simplejson"],
+ entry_points={
+  "console_scripts": [
+    "iosapplist=iosapplist.cli:main"
+  ]
+ },
+)
