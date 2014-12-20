4d8f86f10ec447f1b090b734e1aba45afb2c6a51
Scott Zeid <s@zeid.me>
2014-12-19 21:01:46 -0600

    incrementing version number

--- iosapplist/__init__.py
+++ iosapplist/__init__.py
@@ -32,7 +32,7 @@
 from __future__ import with_statement
 
 __author__  = "Scott Zeid <s@zeid.me>"
-__version__ = "3.0.dev12"
+__version__ = "3.0.dev13"
 
 codename    = "Maserati"  # git push all maserati
 
