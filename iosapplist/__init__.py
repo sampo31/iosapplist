5c7ab8cfa4196ade32d057411225fede302227c6
Scott Zeid <s@zeid.me>
2014-12-20 21:17:35 -0600

    increment version number

--- iosapplist/__init__.py
+++ iosapplist/__init__.py
@@ -32,7 +32,7 @@
 from __future__ import with_statement
 
 __author__  = "Scott Zeid <s@zeid.me>"
-__version__ = "3.0.dev15"
+__version__ = "3.0.dev16"
 
 codename    = "Maserati"  # git push all maserati
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
  name="iosapplist",
- version="3.0.dev15",
+ version="3.0.dev16",
  description="Lists installed iOS App Store apps.",
  url="http://code.s.zeid.me/iosapplist",
  author="Scott Zeid",
