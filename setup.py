3483a8a8f212e17eb5ee52d85641f1f4d87dd35d
Scott Zeid <s@zeid.me>
2014-12-23 08:43:13 -0600

    increment version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
  name="iosapplist",
- version="3.0.dev20",
+ version="3.0.dev21",
  description="Lists installed iOS App Store apps.",
  url="http://code.s.zeid.me/iosapplist",
  author="Scott Zeid",
